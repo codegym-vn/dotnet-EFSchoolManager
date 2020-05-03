1f97a7310ede5ee435b5dde02546e2fd07e9e30e
nguyenkhanhtung <tungnk0711@gmail.com>
2020-05-03 23:23:34 +0700

    first commit

---
+++ Model.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EFSchoolManager
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=SchoolDB.db");
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EFSchoolManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext()) {
+
+                var std = new Student()
+                {
+                     Name = "Bill"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
